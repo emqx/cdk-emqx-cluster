97498c6d183aa542fc156cfaf6e8ffbdf5f119c5
William Yang <mscame@gmail.com>
2021-11-03 10:00:04 +0100

    loadgen: start empd when provision

--- user_data/loadgen_init.sh
+++ user_data/loadgen_init.sh
@@ -2,6 +2,9 @@
 set -euo pipefail
 domain=$(dnsdomainname)
 
+# emqtt bench escript will not start epmd, so we start it here
+epmd -daemon
+
 cd /root/
 git clone -b "master" https://github.com/emqx/emqtt-bench.git
 cd emqtt-bench
