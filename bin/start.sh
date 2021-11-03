dbe5070bdeb464a9307f6a94bda6766ce1b7c3b3
k32 <10274441+k32@users.noreply.github.com>
2021-11-03 13:05:17 +0100

    start.sh: Handle complex git names (remove spaces, punctuation, etc)

--- bin/start.sh
+++ bin/start.sh
@@ -1,6 +1,9 @@
 #/bin/bash
 set -eox pipefail
-[ -z ${CDK_EMQX_CLUSTERNAME+x} ] && CDK_EMQX_CLUSTERNAME=$(git config --get user.name)
+
+[ -z ${CDK_EMQX_CLUSTERNAME+x} ] &&
+    CDK_EMQX_CLUSTERNAME=$(git config --get user.name | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9]//')
+
 docker run -e CDK_EMQX_CLUSTERNAME=${CDK_EMQX_CLUSTERNAME} --rm -it \
        -v ~/.aws:/root/.aws -v $(pwd):/opt/ contino/aws-cdk \
        /opt/bin/container-entrypoint.sh
