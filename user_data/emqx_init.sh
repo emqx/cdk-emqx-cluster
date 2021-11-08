a4908bcccf7288cd4b03c472183f1210026d89e8
William Yang <mscame@gmail.com>
2021-11-08 16:12:56 +0100

    support both emqx 4.x and emqx 5.x

--- user_data/emqx_init.sh
+++ user_data/emqx_init.sh
@@ -26,7 +26,7 @@ maybe_install_from_src() {
   popd
 }
 
-config_overrides() {
+config_overrides_v5() {
   domain=$(dnsdomainname)
   nodename="emqx@`hostname -f`"
   cat <<EOF >> /etc/emqx/emqx.conf
@@ -103,6 +103,24 @@ rate_limit {
 EOF
 }
 
+
+config_overrides_v4() {
+  domain=$(dnsdomainname)
+  echo "## ========= cloud user_data start  ===========##" >> /etc/emqx/emqx.conf
+  echo "node.name = emqx@`hostname -f`" >> /etc/emqx/emqx.conf
+  
+  cat <<EOF >> /etc/emqx/emqx.conf
+cluster.discovery = etcd
+cluster.etcd.server = http://etcd0.${domain}:2379
+listener.tcp.external.max_conn_rate = 5000
+listener.tcp.external.acceptors = 128
+## ========= cloud user_data end  ===========##
+EOF
+
+  echo "prometheus.push.gateway.server = http://lb.${domain}:9091" >> /etc/emqx/plugins/emqx_prometheus.conf
+  echo "{emqx_prometheus, true}." >> /var/lib/emqx/loaded_plugins
+}
+
 # Assume we have emqx src in PWD
 # emqx src is either deb file or git src tree
 
@@ -110,7 +128,19 @@ maybe_mount_data
 maybe_install_from_deb
 maybe_install_from_src
 
-config_overrides
+EMQX_VERSION=$(dpkg -s emqx | grep Version | awk '{print $2}')
+
+case "${EMQX_VERSION}" in
+  4*)
+    config_overrides_v4
+    ;;
+  5*)
+    config_overrides_v5
+    ;;
+  *)
+    echo "Unknown EMQX_VERSION: ${EMQX_VERSION}"
+esac    
+
 
 systemctl start emqx
 
