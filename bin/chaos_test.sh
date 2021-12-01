1fa65bf4f80c82ec539334442dcc4779860c5e6b
William Yang <mscame@gmail.com>
2021-12-01 10:36:31 +0100

    clean chaos_test.sh

--- bin/chaos_test.sh
+++ bin/chaos_test.sh
@@ -23,66 +23,7 @@ gen_tc() {
     $BASEDIR/send_cmd.sh "$cluster" "start_traffic" || die "stopped: start traffic failed"
     # step 2: sleep for 5mins for steady state
     sleep 300;
-    # step 3: send command to shutdown emqx node for 1 min.
-    $BASEDIR/inject_fault.sh "$cluster" "$fault" || die "stopped: fault injection failed"
-    # step 4: wait for traffic to back to normal
-    sleep 300;
-    # step 5: collect logs
-    $BASEDIR/send_cmd.sh "$cluster" "collect_logs" || die "stopped: collect_logs failed"
-
-    echo "test finished"
-}
-
-
-# emqx node stop
-tc_1() {
-    cluster=$1
-    # step 0: stop loadgen
-    $BASEDIR/send_cmd.sh "$cluster" "stop_traffic" || die "stopped: stop traffic failed"
-    # step 1: start traffic
-    $BASEDIR/send_cmd.sh "$cluster" "start_traffic" || die "stopped: start traffic failed"
-    # step 2: sleep for 5mins for steady state
-    sleep 300;
-    # step 3: send command to shutdown emqx node for 1 min.
-    $BASEDIR/inject_fault.sh "$cluster" "emqx-node-shutdown" || die "stopped: fault injection failed"
-    # step 4: wait for traffic to back to normal
-    sleep 300;
-    # step 5: collect logs
-    $BASEDIR/send_cmd.sh "$cluster" "collect_logs" || die "stopped: collect_logs failed"
-
-    echo "test finished"
-}
-
-tc_2() {
-    cluster=$1
-    fault="emqx-high-cpu-80"
-    # step 0: stop loadgen
-    $BASEDIR/send_cmd.sh "$cluster" "stop_traffic" || die "stopped: stop traffic failed"
-    # step 1: start traffic
-    $BASEDIR/send_cmd.sh "$cluster" "start_traffic" || die "stopped: start traffic failed"
-    # step 2: sleep for 5mins for steady state
-    sleep 300;
-    # step 3: send command to shutdown emqx node for 1 min.
-    $BASEDIR/inject_fault.sh "$cluster" "$fault" || die "stopped: fault injection failed"
-    # step 4: wait for traffic to back to normal
-    sleep 300;
-    # step 5: collect logs
-    $BASEDIR/send_cmd.sh "$cluster" "collect_logs" || die "stopped: collect_logs failed"
-
-    echo "test finished"
-}
-
-
-tc_3() {
-    cluster=$1
-    fault="emqx-high-io-80"
-    # step 0: stop loadgen
-    $BASEDIR/send_cmd.sh "$cluster" "stop_traffic" || die "stopped: stop traffic failed"
-    # step 1: start traffic
-    $BASEDIR/send_cmd.sh "$cluster" "start_traffic" || die "stopped: start traffic failed"
-    # step 2: sleep for 5mins for steady state
-    sleep 300;
-    # step 3: send command to shutdown emqx node for 1 min.
+    # step 3: inject faults
     $BASEDIR/inject_fault.sh "$cluster" "$fault" || die "stopped: fault injection failed"
     # step 4: wait for traffic to back to normal
     sleep 300;
@@ -100,8 +41,11 @@ case $tc_name in
      all)
          for fault in ${ALL_FIS_FAULTS[*]};
          do
-             gen_tc "$cluster" "$fault"
+             gen_tc "$cluster" "$fault";
          done
+         ;;
+     list)
+         echo "Available faults: ${ALL_FIS_FAULTS[*]}"
          ;;
      *)
          $tc_name $cluster
