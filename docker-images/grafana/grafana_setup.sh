33c1511e4c6c89697ab7c46d8d664b58caa9a796
Thales Macedo Garitezi <thalesmg@gmail.com>
2021-12-30 16:49:41 -0300

    fix: update dashboard urls

--- docker-images/grafana/grafana_setup.sh
+++ docker-images/grafana/grafana_setup.sh
@@ -3,9 +3,9 @@ set -euo pipefail
 
 mkdir -p /var/lib/grafana/dashboards
 cd /var/lib/grafana/dashboards
-curl -O "https://raw.githubusercontent.com/k32/grafana-dashboards/master/app_top.json"
-curl -O "https://raw.githubusercontent.com/k32/grafana-dashboards/master/emqx_exporter.json"
-curl -O "https://raw.githubusercontent.com/k32/grafana-dashboards/master/node_exporter.json"
-curl -O "https://raw.githubusercontent.com/k32/grafana-dashboards/master/node_exporter_full.json"
-curl -O "https://raw.githubusercontent.com/k32/grafana-dashboards/master/proc_top.json"
-curl -O "https://raw.githubusercontent.com/k32/grafana-dashboards/master/proc_history.json"
+curl -O "https://raw.githubusercontent.com/k32/grafana-dashboards/master/grafana/dashboards/app_top.json"
+curl -O "https://raw.githubusercontent.com/k32/grafana-dashboards/master/grafana/dashboards/emqx_exporter.json"
+curl -O "https://raw.githubusercontent.com/k32/grafana-dashboards/master/grafana/dashboards/node_exporter.json"
+curl -O "https://raw.githubusercontent.com/k32/grafana-dashboards/master/grafana/dashboards/node_exporter_full.json"
+curl -O "https://raw.githubusercontent.com/k32/grafana-dashboards/master/grafana/dashboards/proc_top.json"
+curl -O "https://raw.githubusercontent.com/k32/grafana-dashboards/master/grafana/dashboards/proc_history.json"
